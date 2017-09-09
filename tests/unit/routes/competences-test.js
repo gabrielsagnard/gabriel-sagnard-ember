e460b16ddbd54571e5c2f2856f512071990a9acf
Gabriel Sagnard <gabrielsagnard26@gmail.com>
2017-09-09 10:07:27 +0200

    Add files via upload

---
+++ tests/unit/routes/competences-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:competences', 'Unit | Route | competences', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
